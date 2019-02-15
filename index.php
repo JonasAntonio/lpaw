6fb5374d6fd697e7929a85225657ed7fc02ec015
JonasAntonio <jayv.jonas@gmail.com>
2019-02-15 18:32:27 -0200

    index

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+    <title>LPAW</title>
+</head>
+<body>
+    <br>
+    <div class="container align-middle">
+        <div class="row">
+            <div class="offset-md-3 col-md-6 text-center">
+                <a href="exercicios">Atividades de PHP</a>
+            </div>
+        </div>
+    <div>
+</body>
+</html>
